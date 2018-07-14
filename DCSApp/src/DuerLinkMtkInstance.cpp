7754543f5c47a0bc2456cc3fb4b58c2ff41d9f0d
hertz.wang <wangh@rock-chips.com>
2018-07-14 11:28:27 +0800

    fuction system_fd_closexec
    Close parent's fds before run command in child process,
    similar with O_CLOEXEC.
    
    Change-Id: I92c508b3d5a8a3260e0dc6f71642603679fe6f92
    Signed-off-by: hertz.wang <wangh@rock-chips.com>

--- DCSApp/src/DuerLinkMtkInstance.cpp
+++ DCSApp/src/DuerLinkMtkInstance.cpp
@@ -27,6 +27,10 @@
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
 #include <csignal>
+#include <errno.h>
+#include <paths.h>
+#include <sys/wait.h>
+
 #include "LoggerUtils/DcsSdkLogger.h"
 #include "DCSApp/DuerLinkMtkInstance.h"
 #include "DCSApp/Configuration.h"
@@ -54,13 +58,56 @@ static bool m_pinging = false;
 
 DuerLinkMtkInstance* DuerLinkMtkInstance::m_duerLink_mtk_instance = nullptr;
 
+int system_fd_closexec(const char* command) {
+    int wait_val = 0;
+    pid_t pid = -1;
+
+    if (!command)
+        return 1;
+
+    if ((pid = vfork()) < 0)
+        return -1;
+
+    if (pid == 0) {
+        int i = 0;
+        int stdin_fd = fileno(stdin);
+        int stdout_fd = fileno(stdout);
+        int stderr_fd = fileno(stderr);
+        long sc_open_max = sysconf(_SC_OPEN_MAX);
+        if (sc_open_max < 0) {
+            APP_ERROR("Warning, sc_open_max is unlimited!\n");
+            sc_open_max = 20000; /* enough? */
+        }
+        /* close all descriptors in child sysconf(_SC_OPEN_MAX) */
+        for (; i < sc_open_max; i++) {
+            if (i == stdin_fd || i == stdout_fd || i == stderr_fd)
+                continue;
+            close(i);
+        }
+
+        execl(_PATH_BSHELL, "sh", "-c", command, (char*)0);
+        APP_DEBUG("%s, %d\n", __func__, __LINE__);
+        _exit(127);
+    }
+
+    while (waitpid(pid, &wait_val, 0) < 0) {
+        APP_INFO("%s, errno: %d. This is fine.\n", __func__, errno);
+        if (errno != EINTR) {
+            wait_val = -1;
+            break;
+        }
+    }
+
+    return wait_val;
+}
+
 bool system_command(const char* cmd) {
     pid_t status = 0;
     bool ret_value = false;
 
     APP_INFO("System [%s]", cmd);
 
-    status = system(cmd);
+    status = system_fd_closexec(cmd);
 
     if (-1 == status) {
         APP_ERROR("system failed!");
