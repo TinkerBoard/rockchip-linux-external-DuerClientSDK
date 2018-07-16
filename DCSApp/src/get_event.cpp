c9796cb382edf19c2188c485f4b6385bfa6e5516
Sun ChuanHu <aaron.sun@rock-chips.com>
2018-07-16 09:29:48 +0800

    dueos: add key fun start wifi config
    Change-Id: I167d6464be04bf580cfac63270d1c4a60f5bb4d5
    Signed-off-by: Sun ChuanHu <aaron.sun@rock-chips.com>
    (cherry picked from commit 322c87c0e78a6f5f6a3fd6ab028c6c6acb18dc9a)

--- DCSApp/src/get_event.cpp
+++ DCSApp/src/get_event.cpp
@@ -118,6 +118,7 @@ struct alexa_key support_keys [] = {
     /*modified by cherry.chen*/
     {KEY_MODE/*WAKEUP*/, 0, 1,  enter_wakeup_mode,},
     {KEY_MODE/*WIFI_MODE*/, 1, 1, enter_wifi_mode},
+    {KEY_FN/*WIFI_MODE*/, 1, 1, enter_wifi_mode},
     {KEY_PLAY/*PLAY/PAUSE*/, 0, 1, enter_play_pause},
     {KEY_PLAY/*BT_MODE*/, 1, 1, enter_bt_mode},
     //{KEY_PLAY/*WAKEUP*/, 0, 1,  enter_wakeup_mode,},
